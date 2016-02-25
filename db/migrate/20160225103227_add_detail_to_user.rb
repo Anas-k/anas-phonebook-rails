fbcf699ad6d2d06aded89e580e064c007488931e
Anas-k <khtib.anas@gmail.com>
2016-02-25 11:33:03 +0100

    g migration detail to user

---
+++ db/migrate/20160225103227_add_detail_to_user.rb
@@ -0,0 +1,12 @@
+class AddDetailToUser < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :email, :string
+    add_column :users, :phone, :string
+    add_column :users, :subscribe, :integer
+    add_column :users, :gender, :integer
+    add_column :users, :address, :string
+    add_column :users, :citycode, :string
+    add_column :users, :city, :string
+  end
+end
